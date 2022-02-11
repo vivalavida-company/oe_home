200bf558501ffbfc640f4c42b2809949eda146a0
dbsgh435 <w9520703@naver.com>
2022-02-11 10:42:52 +0900

    오이 소개 페이지 spread버튼 디자인 수정

--- script/event.js
+++ script/event.js
@@ -3,6 +3,7 @@
     const detailBtn = document.querySelector('.js_btnClick');
     const spreadIc = document.querySelector('.js_spreadIc');
     const lineBtnP = document.querySelector('.js_lineBtnP');
+    const oeMain = document.querySelector('.js_oeMain');
 
 
 
@@ -15,15 +16,27 @@ detailBtn.addEventListener('click' , function() {
         lineBtnP.innerHTML = '접기'
         main.classList.remove("js_hToggle")
         
-        spreadIc.classList.remove("ic_spread_down")
-        spreadIc.classList.add("ic_spread_up")
+        spreadIc.classList.remove("ic_spread_down__blue")
+        spreadIc.classList.add("ic_spread_up__blue")
+
+        detailBtn.classList.remove("hp_absolBottom8")
+        detailBtn.classList.add("hp_absolBottom20")
+
+        oeMain.classList.remove("el_topBoldLine")
+        oeMain.classList.add("el_topBoldLine__top0")
         
     } else {
         lineBtnP.innerHTML = '자세히 보기'
         main.classList.add("js_hToggle")
 
-        spreadIc.classList.remove("ic_spread_up")
-        spreadIc.classList.add("ic_spread_down")
+        spreadIc.classList.remove("ic_spread_up__blue")
+        spreadIc.classList.add("ic_spread_down__blue")
+
+        detailBtn.classList.remove("hp_absolBottom20")
+        detailBtn.classList.add("hp_absolBottom8")
+
+        oeMain.classList.remove("el_topBoldLine__top0")
+        oeMain.classList.add("el_topBoldLine")
     }
 
 })
