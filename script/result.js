dffc6f87bbabe6dadbd1d51b80383ef32b2ab0b5
dbsgh435 <dbsgh676@gmail.com>
2024-07-04 11:25:54 +0900

    사용하지 않는 파일 정리

--- script/result.js
+++
@@ -1,6 +0,0 @@
-function apiShare(){
-    fetch('https://oi-server.com/api/promotion/sinsa/share', {method:'post'})
-}
-function apiDownload(){
-    fetch('https://oi-server.com/api/promotion/sinsa/download', {method:'post'})
-}
